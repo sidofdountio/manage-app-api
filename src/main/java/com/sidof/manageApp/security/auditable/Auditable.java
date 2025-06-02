7389901f9310f06535f62af0b7e5710d00b85763
sidof <sidofdountio406@gmail.com>
2025-06-02 13:55:28 +0100

    setting open ai 2

---
+++ src/main/java/com/sidof/manageApp/security/auditable/Auditable.java
@@ -0,0 +1,17 @@
+package com.sidof.manageApp.security.auditable;
+
+/**
+ * <blockquote><pre>
+ * Author   : @Dountio
+ * LinkedIn : @SidofDountio
+ * GitHub   : @SidofDountio
+ * Version  : V1.0
+ * Email    : sidofdountio406@gmail.com
+ * Licence  : All Right Reserved BIS
+ * Since    : 6/2/25
+ * </blockquote></pre>
+ */
+
+
+public class Auditable {
+}
